bacd611e44f46027fa48dd40ca6d8027e35d1305
_YinQ <1339529465@qq.com>
2020-03-23 23:09:25 +0800

    Update ck.sh

--- ck.sh
+++ ck.sh
@@ -1,4 +1,4 @@
-apt install dirmngr
+apt install dirmngr -y
 echo "deb https://assets.checkra.in/debian /" | tee -a /etc/apt/sources.list
 apt-key adv --fetch-keys https://assets.checkra.in/debian/archive.key
 apt update
